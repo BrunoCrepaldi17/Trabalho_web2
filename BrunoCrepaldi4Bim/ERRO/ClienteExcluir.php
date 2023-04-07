6df3a5fe45f41a5a33e4e68de96d56896e447b27
Bruno crepaldi <107934654+BrunoCrepaldi17@users.noreply.github.com>
2023-04-07 14:40:29 -0300

    Delete BrunoCrepaldi4Bim/ERRO directory

--- BrunoCrepaldi4Bim/ERRO/ClienteExcluir.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexao = mysqli_connect('127.0.0.1', 'root', '', 'web2');
-        if (isset($_GET['id'])) {
-          $sql = "delete from cliente where id_cliente = " . $_GET['id'];
-          mysqli_query($conexao, $sql);
-          $mensagem = "registro excluido com sucesso.";
-        }
-
-?>
